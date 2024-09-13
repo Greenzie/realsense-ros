909caffd92f1f6bb6f03c61685a74d3d016559b4
Evan <evan@greenzie.com>
2024-09-13 14:00:58 -0400

    Inital commit, working with librealsense 2.55.1

--- realsense2_camera/include/constants.h
+++ realsense2_camera/include/constants.h
@@ -7,7 +7,7 @@
 
 #define REALSENSE_ROS_MAJOR_VERSION    2
 #define REALSENSE_ROS_MINOR_VERSION    3
-#define REALSENSE_ROS_PATCH_VERSION    2
+#define REALSENSE_ROS_PATCH_VERSION    29
 
 #define STRINGIFY(arg) #arg
 #define VAR_ARG_STRING(arg) STRINGIFY(arg)
@@ -36,6 +36,7 @@ namespace realsense2_camera
     const uint16_t RS416_RGB_PID    = 0x0B52; // F416 RGB
     const uint16_t RS405_PID        = 0x0b0c; // DS5U
     const uint16_t RS455_PID        = 0x0B5C; // D455
+    const uint16_t RS457_PID        = 0xABCD; // D457
     const uint16_t RS_T265_PID      = 0x0b37; // 
     const uint16_t RS_L515_PID_PRE_PRQ = 0x0B3D; // 
     const uint16_t RS_L515_PID      = 0x0B64; // 
